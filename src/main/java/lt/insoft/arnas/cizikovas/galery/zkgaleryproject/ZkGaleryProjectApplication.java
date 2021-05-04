1091eb9b3058d49fc7693a264554b7b828e64402
Arnas Čižikovas <arnas.cizikovas@insoft.lt>
2021-05-04 09:42:36 +0300

    Initial commit

---
+++ src/main/java/lt/insoft/arnas/cizikovas/galery/zkgaleryproject/ZkGaleryProjectApplication.java
@@ -0,0 +1,47 @@
+package lt.insoft.arnas.cizikovas.galery.zkgaleryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.zkoss.zk.au.http.DHtmlUpdateServlet;
+import org.zkoss.zk.ui.http.DHtmlLayoutServlet;
+import org.zkoss.zk.ui.http.HttpSessionListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class ZkGaleryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZkGaleryProjectApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean dHtmlLayoutServlet() {
+		Map<String, String> params = new HashMap<>();
+		params.put("update-uri", "/zkau");
+		ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlLayoutServlet(), "*.zul");
+		reg.setLoadOnStartup(1);
+		reg.setInitParameters(params);
+		return reg;
+	}
+
+	@Bean
+	public ServletRegistrationBean dHtmlUpdateServlet() {
+		Map<String, String> params = new HashMap<>();
+		params.put("update-uri", "/zkau/*");
+		ServletRegistrationBean reg = new ServletRegistrationBean(new DHtmlUpdateServlet(), "/zkau/*");
+		reg.setLoadOnStartup(2);
+		reg.setInitParameters(params);
+		return reg;
+	}
+
+	// ZK listeners
+	@Bean
+	public ServletListenerRegistrationBean<HttpSessionListener> httpSessionListener() {
+		return new ServletListenerRegistrationBean<>(new HttpSessionListener());
+	}
+}
---
+++ src/test/java/lt/insoft/arnas/cizikovas/galery/zkgaleryproject/ZkGaleryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.insoft.arnas.cizikovas.galery.zkgaleryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkGaleryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
