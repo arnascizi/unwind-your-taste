bad9f6aaad7a14b106b586cc6ba5d0d7e0cd8550
Arnas Čižikovas <arnas.cizikovas@insoft.lt>
2021-05-06 22:26:43 +0300

    DependancyManagement

--- gallery-model/src/main/java/lt/insoft/entity/Image.java
+++ gallery-model/src/main/java/lt/insoft/entity/Image.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 
 @Entity
 @Data
@@ -26,6 +25,5 @@ public class Image {
 
     @Column
     private String url;
-
 }
 
