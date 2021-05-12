9befae604fb58532d7088fe5fb005d2e9f8753f8
Arnas Čižikovas <arnas.cizikovas@insoft.lt>
2021-05-12 11:32:14 +0300

    fix: gitignore

--- gallery-ui/src/main/java/lt/insoft/galleryui/GalleryUiApplication.java
+++ gallery-ui/src/main/java/lt/insoft/galleryui/GalleryUiApplication.java
@@ -2,13 +2,9 @@ package lt.insoft.galleryui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.tuckey.web.filters.urlrewrite.UrlRewriteFilter;
 
-@EntityScan(basePackages = {"lt.insoft.gallerymodel"})
-@EnableJpaRepositories("lt.insoft.gallerybl")
 @SpringBootApplication
 public class GalleryUiApplication {
 
--- gallery-ui/src/main/java/lt/insoft/galleryui/viewmodel/ImageVm.java
+++ gallery-ui/src/main/java/lt/insoft/galleryui/viewmodel/ImageVm.java
@@ -1,17 +1,22 @@
 package lt.insoft.galleryui.viewmodel;
 
+import lombok.Getter;
+import lt.insoft.gallerybl.service.ImageService;
+import lt.insoft.gallerymodel.model.Image;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.zk.ui.select.annotation.WireVariable;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class ImageVm implements Serializable {
     private static final long serialVersionUID = -694624904097484135L;
 
-//    @WireVariable(rewireOnActivate = true)
-//    private transient ImageService imageService;
-//
-//    @Getter
-//    private List<Image> imageList;
+    @WireVariable(rewireOnActivate = true)
+    private transient ImageService imageService;
+
+    @Getter
+    private List<Image> imageList;
 
     @Init
     public void init() {
