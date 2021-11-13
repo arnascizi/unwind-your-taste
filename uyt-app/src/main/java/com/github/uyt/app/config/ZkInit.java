abd63b0acfe1f75fcb71dc692196fe50413be96a
Arnas Čižikovas <arnas.cizikovas@gmail.com>
2021-11-13 16:07:31 +0200

    Trijų sluoksnių architektūros paruošimas, pirminių bibliotekų konfigūravimas

---
+++ uyt-app/src/main/java/com/github/uyt/app/config/ZkInit.java
@@ -0,0 +1,10 @@
+package com.github.uyt.app.config;
+
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.util.WebAppInit;
+
+public class ZkInit implements WebAppInit {
+    @Override
+    public void init(WebApp wapp) {
+    }
+}
