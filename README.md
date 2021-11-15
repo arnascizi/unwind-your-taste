57d2f36f0e38c814de46a5cad52599657866b283
Arnas Čižikovas <arnas.cizikovas@insoft.lt>
2021-11-15 09:29:47 +0200

    Projekto pravalymas

---
+++ README.md
@@ -0,0 +1,9 @@
+# Atpalaiduok savo skonį - Unwind your taste
+
+### TODO:
+- Database
+- Back-end
+- Front-end
+- Documentation
+
+##### Baigiamojo darbo projektas - Arnas Čižikovas PI18s. 
---
+++ uyt-app/src/main/java/com/github/uyt/app/config/ZkInit.java
@@ -0,0 +1,44 @@
+package com.github.uyt.app.config;
+
+import java.net.URL;
+import java.util.Arrays;
+import java.util.Locale;
+
+import org.zkoss.util.resource.LabelLocator;
+import org.zkoss.util.resource.Labels;
+import org.zkoss.util.resource.Locator;
+import org.zkoss.util.resource.Locators;
+import org.zkoss.zk.ui.WebApp;
+import org.zkoss.zk.ui.util.WebAppInit;
+
+public class ZkInit implements WebAppInit {
+    @Override
+    public void init(WebApp wapp) {
+        for (String name : Arrays.asList("common")) {
+            Labels.register(new ExtraLabelLocator("./i18n/" + name + ".properties"));
+        }
+    }
+
+    private static class ExtraLabelLocator implements LabelLocator {
+        private static final String ZK_CLASS_PATH_PREFIX = ".";
+
+        private String path;
+
+        public ExtraLabelLocator(String path) {
+            super();
+            this.path = path;
+        }
+
+        @Override
+        public URL locate(Locale locale) {
+            String propertiesPath = path;
+            final int j = propertiesPath.lastIndexOf('.');
+            final String prefix = j >= 0 ? propertiesPath.substring(0, j) : propertiesPath;
+            final String suffix = j >= 0 ? propertiesPath.substring(j) : "";
+            propertiesPath = locale == null ? prefix + suffix : prefix + '_' + locale + suffix;
+
+            final Locator locator = Locators.getDefault();
+            return locator.getResource(propertiesPath.replace(ZK_CLASS_PATH_PREFIX, ""));
+        }
+    }
+}
