fc563d7ea838960693827bede14922d0132da97a
Arnas Čižikovas <arnas.cizikovas@insoft.lt>
2021-11-15 09:27:30 +0200

    Pravalytas projektas nuo nereikalingų failų

--- uyt-app/src/main/java/com/github/uyt/app/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.github.uyt.app.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EntityScan(basePackages = {"com.github.uyt.model"})
-// @EnableJpaRepositories(basePackages = {"com.github.uyt.bl"})
-public class AppConfig {}
