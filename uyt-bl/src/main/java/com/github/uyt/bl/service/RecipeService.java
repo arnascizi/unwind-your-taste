c307f506daf8b11419a7fd26f7928cc07e55fbbc
Arnas Čižikovas <arnas.cizikovas@gmail.com>
2021-11-22 23:48:03 +0200

    Pagrindinis sistemos šablono paruošimas

--- uyt-bl/src/main/java/com/github/uyt/bl/service/RecipeService.java
+++ uyt-bl/src/main/java/com/github/uyt/bl/service/RecipeService.java
@@ -1,21 +1,12 @@
 package com.github.uyt.bl.service;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import com.github.uyt.bl.repository.RecipeRepository;
-import com.github.uyt.model.Recipe;
-
-import lombok.RequiredArgsConstructor;
-
-@Service
-@RequiredArgsConstructor
-public class RecipeService {
-
-    private final RecipeRepository recipeRepository;
-
-    public List<Recipe> fetchAllRecipes() {
-        return recipeRepository.getAllRecipes();
-    }
-}
+// @Service
+// @RequiredArgsConstructor
+// public class RecipeService {
+//
+//     private final RecipeRepository recipeRepository;
+//
+//     public List<Recipe> fetchAllRecipes() {
+//         return recipeRepository.getAllRecipes();
+//     }
+// }
