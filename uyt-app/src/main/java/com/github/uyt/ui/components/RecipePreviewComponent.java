0326260c02bb81331a5715111f413b1635b427e3
Arnas Čižikovas <arnascizi@DESKTOP-TG4649B.localdomain>
2021-12-01 22:49:29 +0200

    Making more UI adjustments

---
+++ uyt-app/src/main/java/com/github/uyt/ui/components/RecipePreviewComponent.java
@@ -0,0 +1,7 @@
+package com.github.uyt.ui.components;
+
+import org.zkoss.zk.ui.HtmlMacroComponent;
+
+public class RecipePreviewComponent extends HtmlMacroComponent {
+    private static final long serialVersionUID = -8541924142904287852L;
+}
