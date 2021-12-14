4132af0afb5e0f049ae92f45f815247d4d91ebf9
Arnas Čižikovas <arnas.cizikovas@gmail.com>
2021-12-14 18:40:10 +0200

    Refactoring

--- uyt-app/src/main/java/com/github/uyt/ui/viewmodel/RecipeEntryVm.java
+++ uyt-app/src/main/java/com/github/uyt/ui/viewmodel/RecipeEntryVm.java
@@ -2,7 +2,10 @@ package com.github.uyt.ui.viewmodel;
 
 import java.io.Serializable;
 
+import javax.print.attribute.standard.Media;
+
 import org.zkoss.bind.annotation.BindingParam;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.zk.ui.select.annotation.WireVariable;
 
@@ -10,7 +13,7 @@ import com.github.uyt.ui.helper.AccountHelper;
 import com.github.uyt.ui.helper.RecipeHelper;
 import com.github.uyt.ui.view.RecipeView;
 
-import jdk.nashorn.internal.objects.annotations.Getter;
+import lombok.Getter;
 
 public class RecipeEntryVm implements Serializable {
     private static final long serialVersionUID= -1813920837374820512L;
