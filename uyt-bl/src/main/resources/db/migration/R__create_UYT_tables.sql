bbb9ec697bdeaab86f00e53a1d2fd74e460aebd5
Arnas Čižikovas <arnas.cizikovas@gmail.com>
2022-01-06 20:42:39 +0200

    heroku fix

--- uyt-bl/src/main/resources/db/migration/R__create_UYT_tables.sql
+++ uyt-bl/src/main/resources/db/migration/R__create_UYT_tables.sql
@@ -1,27 +1,3 @@
-drop schema public cascade;
-create schema if not exists public;
-
-create table flyway_schema_history
-(
-    installed_rank integer                 not null
-        constraint flyway_schema_history_pk
-            primary key,
-    version        varchar(50),
-    description    varchar(200)            not null,
-    type           varchar(20)             not null,
-    script         varchar(1000)           not null,
-    checksum       integer,
-    installed_by   varchar(100)            not null,
-    installed_on   timestamp default now() not null,
-    execution_time integer                 not null,
-    success        boolean                 not null
-);
-
-alter table flyway_schema_history
-    owner to postgres;
-create index flyway_schema_history_s_idx
-    on flyway_schema_history (success);
-
 create sequence if not exists lytis_id_seq maxvalue 2147483647;
 alter sequence lytis_id_seq owner to postgres;
 create table if not exists lytis
